ff9ddbf22a020bfd7423a1273b4db22f67ed81d2
eliezerraj <eliezerraj@gmail.com>
2024-04-30 00:14:01 -0300

    fix minor errors

--- create-model-endconfig-end.py
+++ create-model-endconfig-end.py
@@ -12,19 +12,21 @@ def install_requirements():
     with open('/opt/ml/processing/input/req/requirements.txt', 'r') as f:
         for line in f.readlines():
             install(line.strip())
-            
+
 if __name__ == "__main__":
 
     # Loading enviroment variables...
-    print("Loading enviroment variables... ")
+    print("----------------------------------")
+    print("1. Loading enviroment variables... ")
 
-    model_package_group_name = "CustomerModelPackageGroup"
+    model_package_group_name = "CustomerClassModelPackageGroup"
     role = get_execution_role()
     sm_client = boto3.client(service_name="sagemaker")
     sm_runtime = boto3.client(service_name="sagemaker-runtime")
 
     # find the latest approved model package
-    print("Find the latest approved model package... ")
+    print("-----------------------------------")
+    print("2. Find the latest approved model package... ")
 
     # get a list of approved model packages from the model package group you specified earlier
     approved_model_packages = sm_client.list_model_packages(
@@ -37,9 +39,10 @@ if __name__ == "__main__":
     try:
         latest_approved_model_package_arn = approved_model_packages['ModelPackageSummaryList'][0]['ModelPackageArn']
     except Exception as e:
-        print("===> Failed to retrieve an approved model package :", e)
+        print("===> Failed to retrieve an approved model package:", e)
 
-    print("latest_approved_model_package_arn: ",latest_approved_model_package_arn)
+    print("-----------------------------------")
+    print("3. Latest_approved_model_package_arn: ",latest_approved_model_package_arn)
 
     # retrieve required information about the model
     latest_approved_model_package_descr = sm_client.describe_model_package(ModelPackageName = latest_approved_model_package_arn)
@@ -49,15 +52,19 @@ if __name__ == "__main__":
     # sagemaker image in ecr
     image_uri = latest_approved_model_package_descr['InferenceSpecification']['Containers'][0]['Image']
 
-    print("model_artifact_uri: ", model_artifact_uri)
-    print("image_uri: ", image_uri)
+    print("-----------------------------------")
+    print("4. model_artifact_uri: ", model_artifact_uri)
+    print("5. image_uri: ", image_uri)
 
     # Create the model...
-    print("Create the model... ")
+    print("-----------------------------------")
+    print("6. Create the model... ")
     from time import gmtime, strftime
 
-    model_name = "kmeans-custome-model-v1-" + strftime("%Y-%m-%d-%H-%M-%S", gmtime())
-    print("Model name: " + model_name)
+    model_name = "kmeans-customer-model-v1-" + strftime("%Y-%m-%d-%H-%M-%S", gmtime())
+
+    print("-----------------------------------")
+    print("7. Model name: " + model_name)
 
     #  environment variables
     env_vars = {"SAGEMAKER_CONTAINER_LOG_LEVEL": "20",
@@ -87,19 +94,21 @@ if __name__ == "__main__":
         ExecutionRoleArn=role,
     )
 
-    print("Created Model Arn: " + create_model_response["ModelArn"])
+    print("-----------------------------------")
+    print("8. Created Model Arn: " + create_model_response["ModelArn"])
 
     # Create endpoint configuration...
-    print("Create endpoint configuration... ")
+    print("-----------------------------------")
+    print("9. Create endpoint configuration... ")
 
     model_epc_name = "mlops-epc-customer-model-v1-" + strftime("%Y-%m-%d-%H-%M-%S", gmtime())
 
     endpoint_config_response = sm_client.create_endpoint_config(
-        EndpointConfigName=xgboost_epc_name,
+        EndpointConfigName=model_epc_name,
         ProductionVariants=[
             {
                 "VariantName": "byoVariant",
-                "ModelName": model_epc_name,
+                "ModelName": model_name,
                 "ServerlessConfig": {
                     "MemorySizeInMB": 4096,
                     "MaxConcurrency": 20,
@@ -108,21 +117,27 @@ if __name__ == "__main__":
         ],
     )
 
-    print("Endpoint Configuration Arn: " + endpoint_config_response["EndpointConfigArn"])
+    print("-----------------------------------")
+    print("10. Endpoint Configuration Arn: " + endpoint_config_response["EndpointConfigArn"])
 
     # Create endpoint configuration...
-    print("Create endpoint ... ")
+    print("-----------------------------------")
+    print("11. Create endpoint ... ")
+
     endpoint_name = "kmeans-serverless-ep-customer-model-v1-" + strftime("%Y-%m-%d-%H-%M-%S", gmtime())
 
     create_endpoint_response = sm_client.create_endpoint(
         EndpointName=endpoint_name,
-        EndpointConfigName=xgboost_epc_name,
+        EndpointConfigName=model_epc_name,
     )
 
-    print("Endpoint Arn: " + create_endpoint_response["EndpointArn"])
+    print("-----------------------------------")
+    print("12. Endpoint Arn: " + create_endpoint_response["EndpointArn"])
 
     # wait for endpoint to reach a terminal state (InService) using describe endpoint
-    print("Wait for endpoint to reach a terminal state (InService) using describe endpoint ... ")
+    print("-----------------------------------")
+    print("13. Wait for endpoint to reach a terminal state (InService) using describe endpoint ... ")
+
     import time
 
     describe_endpoint_response = sm_client.describe_endpoint(EndpointName=endpoint_name)
@@ -134,4 +149,5 @@ if __name__ == "__main__":
 
     describe_endpoint_response
 
+    print("-----------------------------------")
     print("Process finished ... ")
--- no-scaled/feature-scaled-no-scaled.py
+++ no-scaled/feature-scaled-no-scaled.py
@@ -19,7 +19,7 @@ def install_requirements():
     with open('/opt/ml/processing/input/req/requirements.txt', 'r') as f:
         for line in f.readlines():
             install(line.strip())
-
+            
 if __name__ == "__main__":
 
     base_dir = "/opt/ml/processing"
@@ -32,7 +32,7 @@ if __name__ == "__main__":
     )
 
     print("-------------------- dataframe data  ----------------------------")
-    print("shape : ", df.shape)
+    print("1. shape : ", df.shape)
 
     #Cleaning and econding
     df_customer = df.filter(['CLIENTNUM',
@@ -46,7 +46,7 @@ if __name__ == "__main__":
                             axis=1)
 
     print("-------------------- df_customera  ----------------------------")
-    print(df_customer.head(3))
+    print("2. df_customers : ", df_customer.head(3))
 
     education_map = {'Uneducated': 0,
                      'Unknown': 0,
@@ -71,43 +71,16 @@ if __name__ == "__main__":
                                'Education_Level_Quality',
                                'Income_Category_Quality']]
 
-    print("-------------------- df_training  ----------------------------")
-    print(df_training.head(3))
+    print("-------------------- train dataframe  ----------------------------")
+    print("3. train_data :", df_training)
 
-    print("-------------------- scaling dataframe  -----------------------")
-
-    scaler = StandardScaler()
-    df_data_scaled = scaler.fit_transform(df_training).astype('float32')
-
-    print("-------------------- save scaler model -----------------------")
-    import s3fs
-
-    bucket_name = "eliezerraj-908671954593-dataset/customer"
-    location_scaler = 's3://{}'.format(bucket_name)
-
-    fs = s3fs.S3FileSystem()
-    output_file = os.path.join(location_scaler, "scaler.joblib")
-
-    with fs.open(output_file, 'wb') as f:
-        joblib.dump(scaler, f)
-
-    print("model saved :", output_file)
-
-    print("-------------------- df_data_scaled  ----------------------------")
-    print(df_data_scaled)
+    print("-------------------- converting to float ----------------------------")
+    df_training32 = df_training.astype('float32')
+    print("4. train_data.float32 :", df_training32)
 
     # Save the Dataframes as csv files
-    # convert array into dataframe
-    print("-------------------- saving dataset  ----------------------------")
-    df_data_scaled_final = pd.DataFrame(df_data_scaled).astype('float32')
-    df_data_scaled_final.to_csv(f"{base_dir}/train/train_data.csv",
-                                header=False,
-                                index=False)
-
-    print("location_customer_encoded_data :", location_scaler)
-
-    df_training.to_csv(f"{location_scaler}/customer_encoded_data.csv",
-                       header=True,
-                       index=False)
+    # convert array into dataframe 
+    # df_data_scaled_final = pd.DataFrame(df_data_scaled).astype('float32')
+    df_training32.to_csv(f"{base_dir}/train/train_data.csv", header=False, index=False)
 
     print("## Processing completed. Exiting.")
